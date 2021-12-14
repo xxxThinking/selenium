30fe053c300b59b87f6597d95f83aaba973e5e11
20171105405 <973986153@qq.com>
2021-12-14 22:56:05 +0800

    流程
    了解框架流程 编写方法

--- python+selenium/config/config_1.py
+++ python+selenium/config/config_1.py
@@ -8,3 +8,5 @@
 driver_file = "G:\selenium\OfficeUpdate.exe\python+selenium\driver\chrome96.0.4664.93 96.0.4664.45\chromedriver.exe"
 url = "http://139.224.113.59/zentao/user-login-L3plbnRhby8=.html"
 file = r"C:\Users\Administrator\Desktop\test.xlsx"
+
+
